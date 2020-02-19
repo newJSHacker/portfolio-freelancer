e57913e83acf76c2a3a9de22459fd39a36ea70d7
Jhon Raymond <horse2019@protonmail.com>
2020-02-19 11:22:29 +0800

    Updates

---
+++ precache-manifest.0d79035f437c2a9d11c4fed355d88227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054988dba25fd69422385060b3018ab2",
+    "url": "/portfolio-freelancer/index.html"
+  },
+  {
+    "revision": "9c34bf19ad311fc7e7bc",
+    "url": "/portfolio-freelancer/static/css/main.06441ff2.chunk.css"
+  },
+  {
+    "revision": "925e713678b3ef52e404",
+    "url": "/portfolio-freelancer/static/js/2.f7f8803b.chunk.js"
+  },
+  {
+    "revision": "7d4610b035b1afebd39e5e8fd5e70ce7",
+    "url": "/portfolio-freelancer/static/js/2.f7f8803b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c34bf19ad311fc7e7bc",
+    "url": "/portfolio-freelancer/static/js/main.b5a6e688.chunk.js"
+  },
+  {
+    "revision": "07dd6acc7f2c8967fd46",
+    "url": "/portfolio-freelancer/static/js/runtime-main.0afcea16.js"
+  },
+  {
+    "revision": "5006b474658013304f9c32f5268f8d00",
+    "url": "/portfolio-freelancer/static/media/picture.5006b474.jpg"
+  }
+]);
