89c8bba6b7f14e0e3f75072a225048425b591692
Jhon Raymond <horse2019@protonmail.com>
2020-02-20 18:57:34 +0800

    Updates

---
+++ precache-manifest.c4a085dd9a3c482b81799ccd1ae6f031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7553583ad3f263813f1d10053ad9941b",
+    "url": "/portfolio-freelancer/index.html"
+  },
+  {
+    "revision": "8a89bb759801f789edbb",
+    "url": "/portfolio-freelancer/static/css/main.14cea85b.chunk.css"
+  },
+  {
+    "revision": "a432caed4360ea844864",
+    "url": "/portfolio-freelancer/static/js/2.97d11d6e.chunk.js"
+  },
+  {
+    "revision": "7d4610b035b1afebd39e5e8fd5e70ce7",
+    "url": "/portfolio-freelancer/static/js/2.97d11d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a89bb759801f789edbb",
+    "url": "/portfolio-freelancer/static/js/main.32f219bc.chunk.js"
+  },
+  {
+    "revision": "07dd6acc7f2c8967fd46",
+    "url": "/portfolio-freelancer/static/js/runtime-main.0afcea16.js"
+  },
+  {
+    "revision": "5006b474658013304f9c32f5268f8d00",
+    "url": "/portfolio-freelancer/static/media/picture.5006b474.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-freelancer/precache-manifest.06d256393e1f50f6b2dcd718f0bb5a88.js"
+  "/portfolio-freelancer/precache-manifest.c4a085dd9a3c482b81799ccd1ae6f031.js"
 );
 
 self.addEventListener('message', (event) => {
